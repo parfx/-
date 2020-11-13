ab5561dbe8eb638d9175ac6b31296ddfdfbf9ed8
unknown <kivin2001@gmail.com>
2020-11-13 14:02:47 +0900

    development2 vinokurov kirill

--- 1zadanie.py
+++ 1zadanie.py
@@ -1,6 +1,31 @@
-n = int(input("Введите число"))
-m = 0
-while n>0:
-    m = m * 10 + n % 10
-    n = n // 10 # делим нацело
-print("Перевернутое число:",m)#добавил текст для понятности
+from tkinter import *
+from tkinter import messagebox
+
+def show_message():
+    messagebox.showinfo("GUI Python", chislo.get())
+
+root = Tk()
+root.title("Simple GUI")
+root.geometry("500x500")
+chislo = numberinv()
+message = IntVar()
+message_entry = Entry(textvariable=message)
+val = message_entry.get()
+int(val)
+message_entry.place(relx=.5, rely=.1, anchor="c")
+def numberinv():
+    n=val.get()
+    m = 0
+    while n>0:
+        m = m * 10 + n % 10
+        n = n // 10 # делим нацело
+        return m
+    #print("Перевернутое число:",m)#добавил текст для понятности   
+
+
+
+btn = Button(text="start", command=show_message)
+btn.pack()
+
+
+root.mainloop()
