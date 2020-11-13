e9e457e5b70ed02d14fa2165203d7209b4968522
Nikolay <fotn25@gmail.com>
2020-11-13 13:10:12 +0900

    рабочий интерфейс для приложения

--- 1zadanie.py
+++ 1zadanie.py
@@ -1,6 +1,19 @@
-n = int(input("Введите число"))
-m = 0
-while n>0:
-    m = m * 10 + n % 10
-    n = n // 10 # делим нацело
-print(m)
+from tkinter import *
+ 
+clicks = 0
+ 
+ 
+def click_button():
+    global clicks
+    clicks += 1
+    root.title("Clicks {}".format(clicks))
+ 
+root = Tk()
+root.title("GUI на Python")
+root.geometry("300x250")
+ 
+btn = Button(text="Click Me", background="#555", foreground="#ccc",
+             padx="20", pady="8", font="16", command=click_button)
+btn.pack()
+ 
+root.mainloop()
