6051982fd0d788b21d136246727fa5596e6ca288
Sergey Parfenov <fanta50fanta9@mail.ru>
2020-11-13 14:10:16 +0900

    Parfenov

--- 1zadanie.py
+++ 1zadanie.py
@@ -1,6 +1,29 @@
-n = int(input("Введите число"))
-m = 0
-while n>0:
-    m = m * 10 + n % 10
-    n = n // 10 # делим нацело
-print(m)
+from tkinter import *
+from tkinter import messagebox
+
+def show_message():
+    messagebox.showinfo("GUI Python", message.get())
+root = Tk()
+root.title("SIMPLE GUI")
+root.geometry("500x500")
+message = IntVar()
+
+message_entry = Entry(textvariable=message)
+message_entry.place(relx=.5, rely=.1, anchor="c")
+
+message_button = Button(text="Click me", command=show_message)
+
+def number(n):
+    m = 0
+    while n > 0:
+        m = m * 10 + n % 10
+        n = m // 10 # делим нацело
+
+        
+
+
+btn = Button(text="Start", command=show_message)
+btn.pack()
+
+
+root.mainloop()
